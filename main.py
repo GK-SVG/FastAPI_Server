5bcded80fcc75f1407a0fe125a0f82ed70a3732a
gk-svg <gk32239@gmail.com>
2021-03-21 20:21:28 +0530

    readme file

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
@@ -10,3 +12,19 @@ def index():
 @app.get('/about')
 def about():
     return {'data':{'about':'i am engineer'}}
+
+
+@app.get('/blog/{id}')
+def blog(id:int):
+    return {'data':{'blog':id}}
+
+
+class Blog(BaseModel):
+    title:str
+    body:str
+    publised_at:Optional[bool]
+
+
+@app.post('/blog/comment')
+def blog(request:Blog):
+    return {'data':{'blog':Blog.title}}
