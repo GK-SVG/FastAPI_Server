b6a7ac2863cb03974b74b13998ef61533823873b
gk-svg <gk32239@gmail.com>
2021-03-22 09:17:44 +0530

    fastapin url parameters and get/post responce

--- blog/main.py
+++ blog/main.py
@@ -5,12 +5,14 @@ from database import engine,SessionLocal
 from sqlalchemy.orm import Session
 
 app = FastAPI()
-
+# print('fastApi Instance--',app)
+# print('fastApi Database engine--',engine)
 
 modals.Base.metadata.create_all(engine)
 
 def get_db():
     db = SessionLocal()
+    # print("get_deb--",db)
     try:
         yield db
     finally:
@@ -19,8 +21,22 @@ def get_db():
 
 @app.post('/blog')
 def create(request:Blog,db:Session = Depends(get_db)):
+    # print("request--",request)
+    # print("db--",db)
     new_blog = modals.Blog(title= request.title,body=request.body)
+    # print("new_blog--",new_blog)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+@app.get('/allBlog')
+def all_blog(db:Session = Depends(get_db)):
+    blogs = db.query(modals.Blog).all()
+    return blogs
+
+
+@app.get('/blog/{id}')
+def blog(id, db:Session = Depends(get_db)):
+    blog = db.query(modals.Blog).filter(modals.Blog.id==id).first()
+    return blog
